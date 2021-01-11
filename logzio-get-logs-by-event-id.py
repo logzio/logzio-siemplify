1e8c68a622fdd7de3b06548e3abaaf5e3f1c3020
mirii1994 <miri.ignatiev@logz.io>
2021-01-11 17:47:14 +0200

    [wip] build request to api

--- logzio-get-logs-by-event-id.py
+++ logzio-get-logs-by-event-id.py
@@ -5,7 +5,8 @@ from ScriptResult import EXECUTION_STATE_COMPLETED, EXECUTION_STATE_FAILED,EXECU
 
 import requests
 
-
+BASE_URL = "https://api.logz.io/"
+SEARCH_LOGS_API_SUFFIX = "v2/security/rules/events/logs/search"
 DEFAULT_PAGE_SIZE = 25
 MIN_PAGE_SIZE = 1
 MAX_PAGE_SIZE = 1000
@@ -20,6 +21,7 @@ def main():
         raise ValueError
     logzio_region = siemplify.extract_configuration_param('Logzio',"logzio_region", default_value="")
     logs_response = execute_logzio_api(siemplify, logzio_token, logzio_region)
+    siemplify.LOGGER.info("{}".format(logs_response))
 
     # status = EXECUTION_STATE_COMPLETED  # used to flag back to siemplify system, the action final status
     # output_message = "output message :"  # human readable message, showed in UI as the action result
@@ -37,33 +39,71 @@ def main():
     
 def execute_logzio_api(siemplify, api_token, logzio_region, page_number=1):
     """ Sends request to Logz.io and returnes the response, if applicable """
+    alert_event_id = siemplify.extract_action_param("alert_event_id", default_value="", is_mandatory=True, print_value=True)
+    if alert_event_id == "":
+        siemplify.LOGGER.error("Error occurred: no alert_event_id! Exiting.")
+        raise ValueError
     try:
         siemplify.LOGGER.info("Fetching page number {}".format(page_number))
-        new_request = create_request_body_obj(siemplify, page_number)
-        new_events = fetch_security_events(api_token, new_request, logzio_region, siemplify)
-        if new_events != None:
-            return new_events
+        new_request = create_request_body_obj(siemplify, alert_event_id, page_number)
+        new_logs = fetch_logs_by_event_id(api_token, new_request, logzio_region, siemplify)
+        if new_logs != None:
+            return new_logs
     except Exception as e:
         siemplify.LOGGER.error("Error occurred while fetching events from page {}: {}".format(page_number, e))
     return None
 
 
-def create_request_body_obj(siemplify, page_number=1):
+def create_request_body_obj(siemplify, alert_event_id, page_number=1):
     """ Creates request to send to Logz.io API """
-    alert_event_id = siemplify.extract_action_param("alert_event_id", default_value="", is_mandatory=True, print_value=True)
-    if alert_event_id == "":
-        siemplify.LOGGER.error("Error occurred: no alert_event_id! Exiting.")
-        raise ValueError
     request_body = {}
     page_size = siemplify.extract_action_param("page_size", is_mandatory=False, default_value=DEFAULT_PAGE_SIZE, input_type=int)
     if page_size < MIN_PAGE_SIZE or page_size > MAX_PAGE_SIZE:
         siemplify.LOGGER.warn("Invalid page size. Should be betwwen {} and {}. Reverting to default page size: {}".format(MIN_PAGE_SIZE, MAX_PAGE_SIZE, DEFAULT_PAGE_SIZE))
         page_size = DEFAULT_PAGE_SIZE
     request_body["filter"] = {}
-    request_body["filter"]["alertEventId"] = alertEventId
+    request_body["filter"]["alertEventId"] = alert_event_id
     request_body["pagination"] = dict(pageNumber=page_number, pageSize=page_size)
     return request_body
     
+    
+def fetch_logs_by_event_id(api_token, req_body, region, siemplify, alert_event_id):
+    """
+    Returnes from Logz.io all the logs that triggered the event.
+    If error occured or no results found, returnes None
+    """
+    headers = {
+        'Content-Type': 'application/json',
+        'X-API-TOKEN': api_token
+    }
+
+    url = get_base_api_url(region) + SEARCH_LOGS_API_SUFFIX
+    siemplify.LOGGER.info("api url: {}".format(url))
+    try:
+        body = json.dumps(req_body)
+        siemplify.LOGGER.info("Fetching logs that triggered event {} from Logz.io".format(alert_event_id))
+        response = requests.post(url, headers=headers, data=body, timeout=5)
+        siemplify.LOGGER.info("Status code from Logz.io: {}".format(response.status_code))
+        if response.status_code == 200:
+            events_response = json.loads(response.content)
+            if events_response["total"] > 0:
+                return events_response
+            siemplify.LOGGER.warn("No resultes found to match your request")
+            return None
+        else:
+            siemplify.LOGGER.error("API request returned {}".format(response.status_code))
+    except Exception as e:
+        siemplify.LOGGER.error("Error occurred while fetching logs that triggered event {} from Logz.io:\n{}".format(alert_event_id, e))
+        return None
+        
+
+def get_base_api_url(region):
+    """ Returnes API url, in accordance to user's input """
+    if region == "us" or region == "" or region == "-":
+        return BASE_URL
+    else:
+        return BASE_URL.replace("api.", "api-{}.".format(region))
+
 
 if __name__ == "__main__":
     main()
