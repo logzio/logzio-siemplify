d4cfda212e1adc96f985dde23430c2c9271db9d2
mirii1994 <miri.ignatiev@logz.io>
2021-01-14 13:41:37 +0200

    [wip] flow done. before testing

--- logzio-get-logs-by-event-id.py
+++ logzio-get-logs-by-event-id.py
@@ -14,28 +14,24 @@ MAX_PAGE_SIZE = 1000
 @output_handler
 def main():
     siemplify = SiemplifyAction()
-
+    status = EXECUTION_STATE_FAILED # default. will be changed only if logs retrieved and parsed to json successfully.
+    num_logs = 0
     logzio_token = siemplify.extract_configuration_param('Logzio',"logzio_token", default_value="-", is_mandatory=True)
     if logzio_token == "-" or logzio_token == "":
         siemplify.LOGGER.error("Error occurred: no Logzio API token! Exiting.")
         raise ValueError
     logzio_region = siemplify.extract_configuration_param('Logzio',"logzio_region", default_value="")
     logs_response = execute_logzio_api(siemplify, logzio_token, logzio_region)
-    # if logs_response is not None:
-    #     logs_json = create_json_result(siemplify, logs_response)
-
-    # status = EXECUTION_STATE_COMPLETED  # used to flag back to siemplify system, the action final status
-    # output_message = "output message :"  # human readable message, showed in UI as the action result
-    # result_value = None  # Set a simple result value, used for playbook if\else and placeholders.
-
-
-    # for entity in siemplify.target_entities:
-    #     print(entity.identifier)
-
-
-
-    # siemplify.LOGGER.info("\n  status: {}\n  result_value: {}\n  output_message: {}".format(status,result_value, output_message))
-    # siemplify.end(output_message, result_value, status)
+    if logs_response is not None:
+        logs_json, num_logs = create_json_result(siemplify, logs_response, logzio_token, logzio_region)
+        if logs_json is not None:
+            siemplify.add_result_json(logs_json)
+            status = EXECUTION_STATE_COMPLETED
+    
+    output_message = get_output_msg(status, num_logs)
+    is_success = status == EXECUTION_STATE_COMPLETED
+    siemplify.end(output_message, is_success, status)
+    
     
     
 def execute_logzio_api(siemplify, api_token, logzio_region, page_number=1):
@@ -106,31 +102,46 @@ def get_base_api_url(region):
         return BASE_URL.replace("api.", "api-{}.".format(region))
 
 
-# def collect_all_logs(siemplify, logs_response, api_token, logzio_region):
-#     collected_logs = logs_response["results"]
-#     num_collected_logs = len(collected_logs)
-#     total_results_available = int(logs_response["total"])
-#     current_page = int(logs_response["pagination"]["pageNumber"])
-#     num_pages = math.ceil(total_results_available/int(logs_response["pagination"]["pageSize"]))
-#     siemplify.LOGGER.info("Request retrieved {} logs from Logz.io".format(num_collected_events))
-#     siemplify.LOGGER.info("There are {} logs in your Logz.io account that match your alert-event-id".format(total_results_available))
-#     with concurrent.futures.ThreadPoolExecutor(max_workers=num_pages) as executor:
-#         futures = []
-#         while num_pages > current_page:
-#             current_page += 1
-#             print("fetching page: {}".format(current_page))
-#             futures.append(executor.submit(execute_logzio_api, siemplify, api_token, logzio_region, current_page))
-#         for future in concurrent.futures.as_completed(futures):
-#             new_event = future.result()
-#             if new_event is not None:
-#                 collected_events += new_event["results"]
-#                 num_collected_events += len(new_event["results"])
-#                 siemplify.LOGGER.info("Fetched {} events".format(len(new_event["results"])))
+def create_json_result(siemplify, logs_response, logzio_token, logzio_region):
+    collected_logs = collect_all_logs(siemplify, logs_response, logzio_token, logzio_token)
+    if collected_logs is not None and len(collected_logs) > 0:
+        return json.dumps(collected_logs), len(collected_logs)
+    return None
+
+
+def collect_all_logs(siemplify, logs_response, api_token, logzio_region):
+    collected_logs = logs_response["results"]
+    num_collected_logs = len(collected_logs)
+    total_results_available = int(logs_response["total"])
+    current_page = int(logs_response["pagination"]["pageNumber"])
+    num_pages = math.ceil(total_results_available/int(logs_response["pagination"]["pageSize"]))
+    siemplify.LOGGER.info("Request retrieved {} logs from Logz.io".format(num_collected_events))
+    siemplify.LOGGER.info("There are {} logs in your Logz.io account that match your alert-event-id".format(total_results_available))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_pages) as executor:
+        futures = []
+        while num_pages > current_page:
+            current_page += 1
+            print("fetching page: {}".format(current_page))
+            futures.append(executor.submit(execute_logzio_api, siemplify, api_token, logzio_region, current_page))
+        for future in concurrent.futures.as_completed(futures):
+            new_log = future.result()
+            if new_log is not None:
+                collected_logs += new_log["results"]
+                num_collected_logs += len(new_log["results"])
+                siemplify.LOGGER.info("Fetched {} events".format(len(new_log["results"])))
         
-#         if total_results_available != num_collected_events:
-#             siemplify.LOGGER.warn("Retrieved {} events out of {} available events. Only the retrieved events will be injected to Siemplify".format(num_collected_events, total_results_available))
-#     siemplify.LOGGER.info("Total collected: {}".format(len(collected_events)))
+        if total_results_available != num_collected_logs:
+            siemplify.LOGGER.warn("Retrieved {} events out of {} available events. Only the retrieved events will be injected to Siemplify".format(num_collected_events, total_results_available))
+    siemplify.LOGGER.info("Total collected: {}".format(len(collected_logs)))
+    return collected_logs
 
 
+def get_output_msg(status, num_logs):
+    if status == EXECUTION_STATE_COMPLETED:
+        return "Retrieved successfully {} logs that triggered the alert".format(num_logs)
+    else:
+        return "Failed to retrieve logs. Please check the script's logs to see what went wrong..."
+    
+    
 if __name__ == "__main__":
     main()
