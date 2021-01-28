9e35bc03d2781039c0aedf433270c39765a9f982
mirii1994 <miri.ignatiev@logz.io>
2021-01-28 19:54:28 +0200

    request to whoami

--- ping.py
+++ ping.py
@@ -2,11 +2,18 @@ from SiemplifyAction import SiemplifyAction
 from SiemplifyUtils import output_handler
 from ScriptResult import EXECUTION_STATE_COMPLETED, EXECUTION_STATE_FAILED
 
+import json
 import requests
-import os
 
 
-BASE_URL = "api.logz.io"
+BASE_URL = "https://api.logz.io/"
+WHOAMI_API_SUFFIX = "v1/account-management/whoami"
+
+"""
+This action validates the tokens and sends a request to Logz.io's API,
+that should return 200 if the token is valid and the connection to the API is established
+"""
+
 
 @output_handler
 def main():
@@ -20,7 +27,8 @@ def main():
     try:
         validate_token(siemplify, security_token)
         validate_token(siemplify, operations_token)
-        ping_api(siemplify, logzio_region)
+        ping_api(siemplify, logzio_region, operations_token)
+        ping_api(siemplify, logzio_region, security_token)
         status = EXECUTION_STATE_COMPLETED
         is_success = True
     except Exception as e:
@@ -31,6 +39,10 @@ def main():
     
     
 def validate_token(siemplify, token):
+    """
+    Minimum validation of the tokens -
+    Checks that they have a value and that they're strings
+    """
     if token is None or token == "-" or token == "":
         raise ValueError("Must insert Logzio operations & security tokens")
     if type(token) is not str:
@@ -40,14 +52,31 @@ def validate_token(siemplify, token):
 
 
 
-def ping_api(siemplify, logzio_region):
-    url = get_base_api_url(logzio_region)
-    response = os.system("ping -c 1 " + url)
-    if response == 0:
-        siemplify.LOGGER.info("ping to {} successful".format(url))
-        return True
-    else:
-        raise ConnectionError("Ping to {} unsuccessful".format(url))
+def ping_api(siemplify, logzio_region, token):
+    """
+    Creates a request to Logz.io API.
+    If request is valid, returnes True, otherwise raises a ConnectionError.
+    """
+    url = get_base_api_url(logzio_region) + WHOAMI_API_SUFFIX
+    headers = {
+        'Content-Type': 'application/json',
+        'X-API-TOKEN': token
+    }
+    
+    try:
+        siemplify.LOGGER.info("Sending request to {}".format(url))
+        response = requests.get(url, headers=headers, timeout=5)
+        if response is not None:
+            siemplify.LOGGER.info("Logz.io returned status code: {}".format(response.status_code))
+            if response.status_code == 200:
+                accountName = json.loads(response.content)
+                siemplify.LOGGER.info("Logz.io response returned account name: {}".format(accountName["accountName"]))
+                return True
+            else:
+                raise ConnectionError("Logz.io returned {}".format(response.status_code))
+        raise ConnectionError("Logz.io response is None")
+    except Exception as e:
+        raise ConnectionError("Error occurred while trying to ping API:\n{}".format(e))
     
 
 def get_base_api_url(region):
@@ -59,6 +88,7 @@ def get_base_api_url(region):
 
 
 def create_output_msg(status):
+    """ Returns output message in accordance to the status """
     if status == EXECUTION_STATE_COMPLETED:
         return "Tokens are valid, ping successful."
     else:
